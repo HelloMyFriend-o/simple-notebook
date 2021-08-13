8b67cbb8e9404a71406f6b243e1df954f995c3ae
GeorgiyTatarishvili <georgy.tatarishvili@gmail.com>
2021-08-13 12:12:21 +0300

    add some fixes

--- snotebook/snotebook/settings.py
+++ snotebook/snotebook/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x9n)u(e4be+&@le51fep_1v4vfi22t4it^!6-pso6mfo-=#rkx'
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
